4c162f18a5f859b0a234d38dfaa480e95a8e589c
KaluluD <ldeng@macalester.edu>
2019-09-28 17:35:17 -0400

    gross data scraping

---
+++ grosses_scrapping.py
@@ -0,0 +1,18 @@
+def getQuote():
+    import urllib.request
+    from bs4 import BeautifulSoup
+
+    myURL = "http://finance.yahoo.com/quote/"
+    stop = False
+    while (stop == False):
+        ticker = input("Enter your stock ticker (enter 'stop' to end the program): ")
+        if ticker != "stop":
+            full_url = myURL + ticker
+            req = urllib.request.Request(full_url)
+            page = urllib.request.urlopen(req)
+            soup = BeautifulSoup(page, "html.parser")
+            price = soup.find("span", {"Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)"}).text
+            print(price)
+        else:
+            print("getQuote() teminated")
+            stop = True
